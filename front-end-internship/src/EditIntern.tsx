fbaa69b421b4540bd55cd60e018594a033715db7
Mtz <mateuszludyk@gmail.com>
2023-01-27 13:37:25 +0100

    edit intern css logic

--- front-end-internship/src/EditIntern.tsx
+++ front-end-internship/src/EditIntern.tsx
@@ -1,25 +1,69 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink, useParams } from "react-router-dom";
 
-export const EditIntern: React.FC = () => {
-  const { id } = useParams();
-
-  useEffect(() => {
-      //TODO: get intern from REST api http://localhost:3001/interns/:id
-      console.log(`I want to get intern with id: ${id}!`)
-  }, [id]);
-
-  return (
-    <div>
-        <NavLink to="/">Back to list </NavLink>
-        <form>
-            <label>Name</label>
-            <input type="text" name="name" />              
-            <label>Email</label>
-            <input type="text" name="email" />
-
-            <input type="submit" value="Submit" />
-        </form>
-    </div>
-  );
+interface InternProps {
+	id: number;
+	name: string;
+	email: string;
+	internshipStart: string;
+	internshipEnd: string;
 }
+
+export const EditIntern: React.FC = () => {
+	const { id } = useParams();
+
+	const [intern, setIntern] = useState<InternProps>();
+
+	const [name, setName] = useState();
+	const [email, setEmail] = useState();
+
+	useEffect(() => {
+		const fetchIntern = async () => {
+			const response = await fetch(`http://localhost:3001/interns/${id}`);
+			const intern = await response.json();
+			setIntern(intern);
+			console.log(intern.name);
+		};
+		fetchIntern();
+	}, [id]);
+
+	if (!intern) return <span className="loading">Loading...</span>;
+	return (
+		<div>
+			<NavLink className="nav" to="/">
+				Back to list
+			</NavLink>
+			<form className="container">
+				<h2>Edit Intern</h2>
+				<label>Full Name *</label>
+				<input
+					required
+					type="text"
+					name="name"
+					defaultValue={intern.name}
+					value={undefined}
+				/>
+				<label>Email address *</label>
+				<input
+					required
+					type="text"
+					name="email"
+					defaultValue={intern.email}
+					value={undefined}
+					onChange={}
+				/>
+				<div className="dates">
+					<div className="start">
+						<label>Work start *</label>
+						<input required className="data" type="date" />
+					</div>
+					<div className="end">
+						<label>Work end *</label>
+						<input required className="data" type="date" />
+					</div>
+				</div>
+				<input className="editButton" type="submit" value="Submit" />
+			</form>
+		</div>
+	);
+};
--- front-end-internship/src/InternList.tsx
+++ front-end-internship/src/InternList.tsx
@@ -2,30 +2,30 @@ import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 interface Intern {
-  id: number;
-  name: string;
-  // ...
+	id: number;
+	name: string;
+	// ...
 }
 
 export const InternList: React.FC = () => {
-  const [interns, setInterns] = useState<Intern[]>([]);
+	const [interns, setInterns] = useState<Intern[]>([]);
 
-  useEffect(() => {
-      const fetchInterns = async () => {
-          const response = await fetch('http://localhost:3001/interns');
-          const interns: Intern[] = await response.json();
-          setInterns(interns);
-      }
-      fetchInterns();
-  }, []);
+	useEffect(() => {
+		const fetchInterns = async () => {
+			const response = await fetch("http://localhost:3001/interns");
+			const interns: Intern[] = await response.json();
+			setInterns(interns);
+		};
+		fetchInterns();
+	}, []);
 
-  return (
-    <div>
-      {interns.map(intern => (
-        <div key={intern.id}>
-          {intern.name} <NavLink to={`/interns/${intern.id}`}>Edit</NavLink>
-        </div>
-      ))}
-    </div>
-  )
-}
+	return (
+		<div>
+			{interns.map((intern) => (
+				<div key={intern.id}>
+					{intern.name} <NavLink to={`/interns/${intern.id}`}>Edit</NavLink>
+				</div>
+			))}
+		</div>
+	);
+};
